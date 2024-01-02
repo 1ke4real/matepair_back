c83c1197862df9513abb516a73bdd2318b38e54c
Mike Leman <mikeeleman@gmail.com>
2024-01-02 10:08:37 +0100

    user easy admin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            yield IdField::new('id')->hideOnForm(),
+            yield TextField::new('username'),
+            yield TextField::new('email'),
+            yield TextField::new('password'),
+            yield TextField::new('role'),
+            yield TextEditorField::new('detail'),
+        ];
+    }
+
+}
