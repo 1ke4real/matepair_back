802d814fa2218da69aef40b837014dad029a85e4
Mike Leman <mikeeleman@gmail.com>
2024-01-02 10:53:23 +0100

    notification date

--- src/Controller/Admin/NotificationCrudController.php
+++ src/Controller/Admin/NotificationCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Notification;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -22,7 +23,7 @@ class NotificationCrudController extends AbstractCrudController
         return [
            yield IdField::new('id')->hideOnForm(),
             yield TextField::new('content'),
-            yield TextField::new('timestamp'),
+            yield DateTimeField::new('timestamp'),
             yield AssociationField::new('user_notif')
         ];
     }
